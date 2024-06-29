5c58680e9d6f5fca6118ca9d6ac0486b2b7318ca
VengeanceIsHis <elijahjpoole@gmail.com>
2024-06-29 03:33:56 -0500

    test for updated at

--- models/base_model.py
+++ models/base_model.py
@@ -11,12 +11,10 @@ class BaseModel:
         if not kwargs:
             from models import storage
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.created_at = str(datetime.now())
+            self.updated_at = str(datetime.now())
             storage.new(self)
         else:
-            kwargs['updated_at'] = str(kwargs['updated_at'])
-            kwargs['created_at'] = str(kwargs['created_at'])
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
             kwargs['created_at'] = datetime.strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
 
