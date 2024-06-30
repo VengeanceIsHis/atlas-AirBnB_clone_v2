5e982d2d773cb9d5bd9614821dce54b6e6516563
VengeanceIsHis <elijahjpoole@gmail.com>
2024-06-29 21:29:44 -0500

    Adding name to the attributes

--- models/city.py
+++ models/city.py
@@ -7,6 +7,7 @@ class City(BaseModel):
     """ The city class, contains state ID and name """
     def __init__(self, **kwargs):
         self.id = str(uuid.uuid4())
+        self.name = kwargs.get('name', None)
         self.state_id = kwargs.get('state_id', None)
         self.created_at = str(datetime.now())
         self.updated_at = str(datetime.now())
