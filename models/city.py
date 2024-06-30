d81e70996a445ad76bd27fe022753a8d0260a83c
LJThao <yengfong.thao@atlasschool.com>
2024-06-30 17:36:04 -0500

    update city

--- models/city.py
+++ models/city.py
@@ -2,13 +2,18 @@
 """ City Module for HBNB project """
 
 
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 import uuid
 from datetime import datetime
+from sqlalchemy import Column, String, ForeignKey
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
+    __tablename__ = 'cities' 
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+
     def __init__(self, **kwargs):
         self.id = str(uuid.uuid4())
         self.name = kwargs.get('name', None)
