fc6401bce62dd062db72f8c8fa861fc17b3ea8bd
LJThao <yengfong.thao@atlasschool.com>
2024-06-30 18:48:11 -0500

    update __init__

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
+
+
 from models.engine.file_storage import FileStorage
+import os
 
+storage_selection = os.getenv('HBNB_TYPE_STORAGE', 'file')
 
-storage = FileStorage()
+if storage_selection == 'db':
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+
 storage.reload()
