14be98f98281385da0617606fef6f7652a5eaea9
LJThao <yengfong.thao@atlasschool.com>
2024-06-30 18:28:34 -0500

    update db_storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,13 +1,59 @@
 #!/usr/bin/python3
-"""This module defines a class to manage the SQL db for hbnb clone"""
+"""This module defines a class to manage the MySQL db for hbnb clone"""
 
 
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
+from models.base_model import Base
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class DBStorage:
-    """This class manages storage of the DB"""
+    """This class manages storage of the MySQL DB"""
     __engine = None
     __session = None
+
+    def __init__(self):
+        """Initialize a DB instance and connects to it"""
+        HBNB_MYSQL_USER = os.getenv('HBNB_MYSQL_USER')
+        HBNB_MYSQL_PWD = os.getenv('HBNB_MYSQL_PWD')
+        HBNB_MYSQL_HOST = os.getenv('HBNB_MYSQL_HOST')
+        HBNB_MYSQL_DB = os.getenv('HBNB_MYSQL_DB')
+        HBNB_ENV = os.getenv('HBNB_ENV')
+
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
+                                      format(HBNB_MYSQL_USER,
+                                             HBNB_MYSQL_PWD,
+                                             HBNB_MYSQL_HOST,
+                                             HBNB_MYSQL_DB), pool_pre_ping=True)
+
+        if HBNB_ENV == "test":
+            Base.metadata.drop_all(self.__engine)
+
+        self.reload()
+
+    def all(self, cls=None):
+        """Query on and return all objects"""
+        new_dict = {}
+        classes = {
+            "User": User,
+            "State": State,
+            "City": City,
+            "Amenity": Amenity,
+            "Place": Place,
+            "Review": Review
+            }
+
+        for class_name in classes:
+            if cls is None or cls is classes[class_name] or cls is class_name:
+                objs = self.__session.query(classes[class_name]).all()
+                for obj in objs:
+                    key = obj.__class__.__name__ + '.' + obj.id
+                    new_dict[key] = obj
+        return new_dict
