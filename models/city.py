f48c6108c52fec782cf728e34a5d5f9a27b5f790
VengeanceIsHis <elijahjpoole@gmail.com>
2024-06-29 21:21:32 -0500

    adding creation times attributes

--- models/city.py
+++ models/city.py
@@ -8,3 +8,5 @@ class City(BaseModel):
     def __init__(self, **kwargs):
         self.id = str(uuid.uuid4())
         self.state_id = kwargs.get('state_id', None)
+        self.created_at = str(datetime.now())
+        self.updated_at = str(datetime.now())
