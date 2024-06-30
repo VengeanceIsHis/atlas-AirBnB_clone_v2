6d70a712293772b3b48242624bf97de112a0c5fe
VengeanceIsHis <elijahjpoole@gmail.com>
2024-06-29 20:29:17 -0500

    Adding state information

--- models/state.py
+++ models/state.py
@@ -5,5 +5,10 @@ import uuid
 
 class State(BaseModel):
     """ State class """
-    def __init__(self):
+    def __init__(self, *args):
         self.id = str(uuid.uuid4())
+        self.name = find_name(args[2])
+def find_name(string):
+        parts = string.split('=')
+        if len(parts) >= 2:
+            return parts[1].strip()
