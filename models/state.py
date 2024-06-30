6f9c48236582b764fb1657fc957ad49d2b9643e9
VengeanceIsHis <elijahjpoole@gmail.com>
2024-06-29 20:58:31 -0500

    adding created at and updated at

--- models/state.py
+++ models/state.py
@@ -8,3 +8,5 @@ class State(BaseModel):
     def __init__(self, **kwargs):
         self.id = str(uuid.uuid4())
         self.name = kwargs.get('name', None)
+        self.created_at = str(datetime.now())
+        self.updated_at = str(datetime.now())
