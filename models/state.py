d92aa6c315bc20dc9f69313426576785fb69b51f
LJThao <yengfong.thao@atlasschool.com>
2024-07-02 16:27:42 +0000

    Revert "updated state"

--- models/state.py
+++ models/state.py
@@ -28,6 +28,19 @@ class State(BaseModel, Base):
                     city_list.append(city)
             return (city_list)
 
-    def __init__(self, *args, **kwargs):
-        """initializes state"""
-        super().__init__(*args, **kwargs)
+    def __init__(self, **kwargs):
+        self.id = str(uuid.uuid4())
+        self.name = kwargs.get('name', None)
+        self.created_at = str(datetime.now())
+        self.updated_at = str(datetime.now())
+
+    def to_dict(self):
+        """Convert instance into dict format"""
+        dictionary = {
+            '__class__': type(self).__name__,
+            'id': self.id,
+            'name': self.name,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at
+        }
+        return (dictionary)
