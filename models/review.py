80c9614ce90ebd7a1f769688eae9f963550f02a3
VengeanceIsHis <elijahjpoole@gmail.com>
2024-07-02 20:22:16 -0500

    Fixing REview error

--- models/review.py
+++ models/review.py
@@ -10,13 +10,10 @@ from sqlalchemy import Column, String, ForeignKey
 
 class Review(BaseModel, Base):
     """ Review classto store review information """
-    __table__ = 'reviews'
+    __tablename__ = 'reviews'
     text = Column(String(1024), nullable=False)
     place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
 
     def __init__(self):
         self.id = str(uuid.uuid4())
-    place_id = ""
-    user_id = ""
-    text = ""
